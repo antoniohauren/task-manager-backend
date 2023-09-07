08af9f4a026ed842f576d757f2a7e0dac0bbe939
antoniohauren <antoniohauren@protonmail.com>
2023-09-07 11:55:05 -0300

    test(user): add use cases to e2e

--- src/user/user.e2e-spec.ts
+++ src/user/user.e2e-spec.ts
@@ -1,12 +1,50 @@
 import { HttpStatus } from '@nestjs/common';
+import { objectBuilder } from '@tests/test.helpers';
+import { execSync } from 'child_process';
 import * as pactum from 'pactum';
+import { CreateUserDto } from './dto';
 
 describe('platform e2e', () => {
   beforeAll(async () => {
-    // execSync('npm run prisma:reset');
+    execSync('npm run prisma:reset');
   });
 
   it('should throw 404 error if user not found', async () => {
     return pactum.spec().get('/user/qwe').expectStatus(HttpStatus.NOT_FOUND);
+  });
+
+  it('should throw 409 error if email already in use', async () => {
+    return pactum
+      .spec()
+      .post('/user')
+      .withJson(
+        objectBuilder<CreateUserDto>({
+          email: 'alice@gmail.com',
+          name: 'teste',
+        }),
+      )
+      .expectStatus(HttpStatus.CONFLICT);
+  });
+
+  it('should throw 400 error if dto is not valid', async () => {
+    return pactum
+      .spec()
+      .post('/user')
+      .withJson({})
+      .expectStatus(HttpStatus.BAD_REQUEST);
+  });
+
+  it('should create a user', async () => {
+    return pactum
+      .spec()
+      .post('/user')
+      .withJson(
+        objectBuilder<CreateUserDto>({
+          email: 'antonio@gmail.com',
+          name: 'Antonio',
+        }),
+      )
+      .expectStatus(HttpStatus.CREATED)
+      .expectBodyContains('antonio@gmail.com');
   });
 });
